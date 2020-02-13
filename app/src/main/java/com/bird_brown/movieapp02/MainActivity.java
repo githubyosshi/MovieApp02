8d8c842d46b691b51a75e8020692afb84964606a
githubyosshi <kanrisya.shisutemu@gmail.com>
2020-02-13 09:45:10 +0900

    Function implementation commit

--- app/src/main/java/com/bird_brown/movieapp02/MainActivity.java
+++ app/src/main/java/com/bird_brown/movieapp02/MainActivity.java
@@ -1,6 +1,9 @@
 package com.bird_brown.movieapp02;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //動画ファイルの場所を設定
+        String uri = "https://www.wowow.co.jp/naomiosakamovie/";
+
+        //VideoViewのオブジェクトを取得
+        VideoView video = (VideoView)findViewById(R.id.videoView1);
+
+        //VideoViewにフォーカスをあてる
+        video.requestFocus();
+
+        //VideoViewにMediaControllerを設定
+        video.setMediaController(new MediaController(this));
+
+        //動画ファイルを読み込み設定
+        video.setVideoURI(Uri.parse(uri));
+
+        //動画再生
+        video.start();
     }
 }
